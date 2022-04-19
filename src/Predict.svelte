b10423e0ef06ce98d40fa7a0a49a276ea8d22a74
James Pettit <etherealmachine@gmail.com>
2022-04-18 21:36:25 -0700

    Simplify and play with grid layout

--- src/Predict.svelte
+++ src/Predict.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
+  import Sortable, { SortableEvent } from "sortablejs";
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
 
-  import SortableList from "./SortableList.svelte";
   import type { Prediction } from "./Types";
 
   export let prediction: Prediction[];
@@ -11,8 +11,21 @@
     return name.split(" ")[1];
   }
 
-  function predictionChanged(event: CustomEvent) {
-    dispatch("change", event.detail);
+  let sortable: Sortable;
+  let sortableOptions = {
+    animation: 200,
+    onSort: (event: SortableEvent) => {
+      const item = prediction.splice(event.oldIndex, 1)[0];
+      prediction = prediction
+        .slice(0, event.newIndex)
+        .concat([item])
+        .concat(prediction.slice(event.newIndex, prediction.length));
+      dispatch("change", prediction);
+    },
+  };
+  let listElement: HTMLElement;
+  $: if (listElement && !sortable) {
+    sortable = Sortable.create(listElement, sortableOptions);
   }
 </script>
 
@@ -20,20 +33,20 @@
   <h3 style="margin-bottom: 8px; text-decoration: underline;">
     Your Prediction
   </h3>
-  <SortableList
-    sortableOptions={{ animation: 200 }}
-    bind:items={prediction}
-    let:item
-    let:index
-    on:change={predictionChanged}
-    ulClass="driver-list"
-    liClass="driver-slot"
-  >
-    <div class="driver-item">
-      <span>{index + 1}</span>
-      <span>{formatName(item.driver.name)}</span>
+  <div>
+    <div class="header">
+      <span>In the Points</span>
+      <span>In the Hunt</span>
     </div>
-  </SortableList>
+    <div bind:this={listElement} class="driver-list">
+      {#each prediction as item, index (item.driver.name)}
+        <div class="driver-item">
+          <span>{index + 1}</span>
+          <span>{formatName(item.driver.name)}</span>
+        </div>
+      {/each}
+    </div>
+  </div>
 </main>
 
 <style>
@@ -42,20 +55,19 @@
     flex-direction: column;
   }
 
-  :global(.driver-list) {
+  .header {
     display: flex;
-    flex-direction: column;
-    list-style-type: none;
-    padding: 0;
-    margin: 0;
+    justify-content: space-around;
+    font-weight: 600;
   }
 
-  :global(.driver-slot) {
-    display: flex;
-    align-items: center;
-    outline: 1px solid black;
-    background: white;
-    padding: 0 16px;
+  .driver-list {
+    display: grid;
+    grid-auto-flow: column;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: repeat(10, auto);
+    padding: 0;
+    margin: 0;
   }
 
   .driver-item {
@@ -63,6 +75,9 @@
     display: flex;
     align-items: center;
     justify-content: space-between;
+    background: white;
+    outline: 1px solid black;
+    padding: 0 16px;
   }
 
   .driver-item > span {
